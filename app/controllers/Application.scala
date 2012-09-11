826cae664cf40677138e481a3042c4bf7a7fb800
Dinduks <samy@dindane.com>
2012-09-11 14:04:50 +0200

    Add the repos search

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,24 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.ws._
+import play.api.libs.json._
 
 object Application extends Controller {
-
   def index = Action {
     Ok(views.html.index())
   }
 
+  def search(keyword: String) = Action {
+    val githubApiUrl = Play.current.configuration.getString("github.apiUrl").get
+    val githubSearchReposPath = Play.current.configuration.getString("github.searchReposPath").get
+
+    Async {
+      WS.url(githubApiUrl + githubSearchReposPath + keyword).get().map { response =>
+        val repos = (response.json \ "repositories")
+        Ok(views.html.searchResults(repos.as[List[JsValue]], keyword))
+      }
+    }
+  }
 }
+
